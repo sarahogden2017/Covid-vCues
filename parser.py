ab5472209a987be0110b8bed5133b5597a750115
Sarah Ogden <sarahogden2017@gmail.com>
2024-05-11 20:29:02 -0400

    updated url parsing

--- parser.py
+++ parser.py
@@ -13,8 +13,8 @@ def parse_news_csv(file, output):
         print(file)
         reader = csv.reader(f)
         # extract the URL column
-        for row in reader:
-            url = row[2]
+        for line in reader:
+            url = line[3]
             with open(output, "a") as out_file:
                 out_file.write(url + "\n")
 
@@ -40,12 +40,21 @@ def main():
     for root, dirs, files in os.walk(CoAID_path):
         for file in files:
             path = os.path.join(root, file)
-            if 'git' not in path and 'README.md' not in path:
-                output_file = 'outputFake.txt' if 'Fake' in path else 'outputReal.txt'
-                if 'tweet' in path:
-                    parse_tweet_csv(path, output_file)
-                else:
-                    parse_news_csv(path, output_file)
+            if 'Real' in path and 'tweet' in path:
+                parse_tweet_csv(path, 'TweetRealUrl.txt')
+            elif 'Fake' in path and 'tweet' in path:
+                parse_tweet_csv(path, 'TweetFakeUrl.txt')
+            elif 'NewsReal' in path:
+                parse_news_csv(path, 'NewsRealUrl.txt')
+            elif 'NewsFake' in path:
+                parse_news_csv(path, 'NewsFakeUrl.txt')
+            else:
+                continue
 
+    # remove duplicates
+    os.system("sort NewsRealUrl.txt | grep -v 'news_url' | uniq > NewsRealUrl_tmp.txt && mv NewsRealUrl_tmp.txt NewsRealUrl.txt")
+    os.system("sort NewsFakeUrl.txt | grep -v 'news_url' | uniq > NewsFakeUrl_tmp.txt && mv NewsFakeUrl_tmp.txt NewsFakeUrl.txt")
+    os.system("sort TweetRealUrl.txt | grep -v 'tweetid' | uniq > TweetRealUrl_tmp.txt && mv TweetRealUrl_tmp.txt TweetRealUrl.txt")
+    os.system("sort TweetFakeUrl.txt | grep -v 'tweetid' | uniq > TweetFakeUrl_tmp.txt && mv TweetFakeUrl_tmp.txt TweetFakeUrl.txt")
 if __name__ == '__main__':
     main()
