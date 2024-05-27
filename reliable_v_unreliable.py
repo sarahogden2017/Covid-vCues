a41444ee64200cfba322b08c5cb0540ed8b789a1
Sarah Ogden <sarahogden2017@gmail.com>
2024-05-27 11:44:15 -0400

    started keras CNN and reorganized folders

---
+++ reliable_v_unreliable.py
@@ -0,0 +1,9 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+img_width, img_height = 224, 224
+
+
