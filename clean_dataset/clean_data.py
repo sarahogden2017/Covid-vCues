68ae27f98061939d3d5c2fa200c0916670459a7c
Sarah Ogden <sarahogden2017@gmail.com>
2024-06-22 17:33:47 -0400

    idk

--- clean_dataset/clean_data.py
+++ clean_dataset/clean_data.py
@@ -1,2 +1,11 @@
 import fastdup
+from IPython.display import HTML
+
+
+if __name__ == "__main__":
+    fastdup.create_duplicates_gallery(similarity_file="reports/similarity.csv", 
+                                      save_path="reports", 
+                                      num_images=5)
+
+    HTML('scene_classification/report/train/similarity.html')
 
